bb083ab84e28a49f321f7b1b2ab90cbddc7b890e
Abhishek Thakur <abhishek.thakur.mainacc@gmail.com>
2025-03-29 22:22:46 +0530

    Update LoadController.java updating routes according to description

--- src/main/java/com/example/loadbooking/controller/LoadController.java
+++ src/main/java/com/example/loadbooking/controller/LoadController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/loads")
+@RequestMapping("load")
 public class LoadController {
 
     private final LoadService loadService;
@@ -19,35 +19,35 @@ public class LoadController {
     }
 
     // Create a new Load
-    @PostMapping("/createLoad")
+    @PostMapping
     public ResponseEntity<Load> createLoad(@RequestBody Load load) {
         Load createdLoad = loadService.createLoad(load);
         return ResponseEntity.ok(createdLoad);
     }
 
     // Get all Loads
-    @GetMapping("/getAllLoads")
+    @GetMapping
     public ResponseEntity<List<Load>> getAllLoads() {
         List<Load> loads = loadService.getAllLoads();
         return ResponseEntity.ok(loads);
     }
 
     // Get Load by ID
-    @GetMapping("getLoadbyId/{id}")
+    @GetMapping
     public ResponseEntity<Load> getLoadById(@PathVariable UUID id) {
         Load load = loadService.getLoadById(id);
         return ResponseEntity.ok(load);
     }
 
     // Update Load
-    @PutMapping("updateLoadByid/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<Load> updateLoad(@PathVariable UUID id, @RequestBody Load updatedLoad) {
         Load load = loadService.updateLoad(id, updatedLoad);
         return ResponseEntity.ok(load);
     }
 
     // Delete Load
-    @DeleteMapping("deleteLoadbyId/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteLoad(@PathVariable UUID id) {
         loadService.deleteLoad(id);
         return ResponseEntity.noContent().build();
