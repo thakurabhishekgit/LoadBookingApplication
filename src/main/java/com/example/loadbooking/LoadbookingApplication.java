1917876c8eb9e6fd9cba75afcb72bbb3dfd77aae
Abhishek Thakur <abhishek.thakur.mainacc@gmail.com>
2025-04-28 22:39:26 +0530

    Updated LoadbookingApplication.java

--- src/main/java/com/example/loadbooking/LoadbookingApplication.java
+++ src/main/java/com/example/loadbooking/LoadbookingApplication.java
@@ -2,7 +2,7 @@ package com.example.loadbooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class LoadbookingApplication {
 
